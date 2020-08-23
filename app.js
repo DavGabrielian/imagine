35fea77fbc84caab247495438f85a2ee73db00ba
David Gabrielian <davidgabrielofficial@gmail.com>
2020-08-23 16:45:16 +0400

    final

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 openNav = () => {
   (document.getElementById("menu").style.display = "none") &&
-    (document.getElementById("navbar").style.display = "flex");
+    (document.getElementById("navbar").style.display = "flex") &&
+    (document.getElementById("closeMenu").style.display = "block");
 };
+
 closeNav = () => {
   (document.getElementById("navbar").style.display = "none") &&
-    (document.getElementById("menu").style.display = "block");
+    (document.getElementById("menu").style.display = "block") &&
+    (document.getElementById("closeMenu").style.display = "none");
 };
